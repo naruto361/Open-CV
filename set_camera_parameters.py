90a3e36fe684a4a775e4e4f80a985add913ffacb
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-09-24 00:48:04 +0530

    Setting Camera Parameters

---
+++ set_camera_parameters.py
@@ -0,0 +1,17 @@
+import cv2
+cap=cv2.VideoCapture(0);
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+cap.set(3,1208)
+cap.set(4,720)
+# 3 for width and 4 for height
+print(cap.get(3))
+print(cap.get(4))
+while(cap.isOpened()):
+    ret,frame=cap.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0XFF==ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
