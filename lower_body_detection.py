2fe42a2b32ef97e4302fdeab5c02af601aa155f4
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-09-25 15:45:12 +0530

    Lower And Upper Body Detection
    Place vtest.avi video in same folder as the codes.

---
+++ lower_body_detection.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import numpy as np
+
+#upper_cascade=cv.CascadeClassifier('haarcascade_upperbody.xml')
+lower_cascade=cv.CascadeClassifier('haarcascade_lowerbody.xml')
+cap=cv.VideoCapture('vtest.avi')
+
+while 1:
+    _,frame=cap.read()
+    gray=cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    lower=lower_cascade.detectMultiScale(gray,1.1,4)
+    for (x,y,w,h) in lower:
+        cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        
+    
+    cv.imshow('frame',frame)
+    k=cv.waitKey(1)
+    if k==27:
+        break
+cap.release()
+cv.destroyAllWindows()
+        
+    
